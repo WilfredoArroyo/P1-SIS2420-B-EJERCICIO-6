7995a1bd3a5302d16dfe26a04ffce4bd03bae68d
WilfredoArroyo <warroyotola@gmail.com>
2023-04-17 10:50:08 -0400

    ejercicio 6 primera practica

---
+++ Ejercicio2/Programa.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ejercicio2
+{
+    partial class Frm_P6
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbx_num = new System.Windows.Forms.ListBox();
+            this.btn_genenar = new System.Windows.Forms.Button();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.Cbbox_operaciones = new System.Windows.Forms.ComboBox();
+            this.lbl_principal = new System.Windows.Forms.Label();
+            this.lbl_listbox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbx_num
+            // 
+            this.lbx_num.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbx_num.FormattingEnabled = true;
+            this.lbx_num.ItemHeight = 26;
+            this.lbx_num.Location = new System.Drawing.Point(155, 196);
+            this.lbx_num.Name = "lbx_num";
+            this.lbx_num.Size = new System.Drawing.Size(437, 212);
+            this.lbx_num.TabIndex = 0;
+            // 
+            // btn_genenar
+            // 
+            this.btn_genenar.BackColor = System.Drawing.Color.Red;
+            this.btn_genenar.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_genenar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_genenar.Location = new System.Drawing.Point(609, 91);
+            this.btn_genenar.Name = "btn_genenar";
+            this.btn_genenar.Size = new System.Drawing.Size(150, 59);
+            this.btn_genenar.TabIndex = 1;
+            this.btn_genenar.Text = "GENERAR";
+            this.btn_genenar.UseVisualStyleBackColor = false;
+            this.btn_genenar.Click += new System.EventHandler(this.btn_genenar_Click);
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_numero.Location = new System.Drawing.Point(239, 104);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(100, 34);
+            this.txt_numero.TabIndex = 2;
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titulo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_titulo.Location = new System.Drawing.Point(12, 107);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(202, 26);
+            this.lbl_titulo.TabIndex = 3;
+            this.lbl_titulo.Text = "iNGRESAR VALOR";
+            // 
+            // Cbbox_operaciones
+            // 
+            this.Cbbox_operaciones.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cbbox_operaciones.FormattingEnabled = true;
+            this.Cbbox_operaciones.Location = new System.Drawing.Point(356, 104);
+            this.Cbbox_operaciones.Name = "Cbbox_operaciones";
+            this.Cbbox_operaciones.Size = new System.Drawing.Size(236, 34);
+            this.Cbbox_operaciones.TabIndex = 4;
+            // 
+            // lbl_principal
+            // 
+            this.lbl_principal.AutoSize = true;
+            this.lbl_principal.Font = new System.Drawing.Font("ISOCPEUR", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_principal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_principal.Location = new System.Drawing.Point(315, 25);
+            this.lbl_principal.Name = "lbl_principal";
+            this.lbl_principal.Size = new System.Drawing.Size(186, 46);
+            this.lbl_principal.TabIndex = 5;
+            this.lbl_principal.Text = "PROGRAMA";
+            // 
+            // lbl_listbox
+            // 
+            this.lbl_listbox.AutoSize = true;
+            this.lbl_listbox.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_listbox.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_listbox.Location = new System.Drawing.Point(23, 152);
+            this.lbl_listbox.Name = "lbl_listbox";
+            this.lbl_listbox.Size = new System.Drawing.Size(227, 26);
+            this.lbl_listbox.TabIndex = 6;
+            this.lbl_listbox.Text = "LOS NUMEROS SON:";
+            // 
+            // frm_P6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_listbox);
+            this.Controls.Add(this.lbl_principal);
+            this.Controls.Add(this.Cbbox_operaciones);
+            this.Controls.Add(this.lbl_titulo);
+            this.Controls.Add(this.txt_numero);
+            this.Controls.Add(this.btn_genenar);
+            this.Controls.Add(this.lbx_num);
+            this.Name = "frm_P6";
+            this.Text = "EJERCICIO 6";
+            this.Load += new System.EventHandler(this.frm_programa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbx_num;
+        private System.Windows.Forms.Button btn_genenar;
+        private System.Windows.Forms.TextBox txt_numero;
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.ComboBox Cbbox_operaciones;
+        private System.Windows.Forms.Label lbl_principal;
+        private System.Windows.Forms.Label lbl_listbox;
+    }
+}
+
---
+++ Ejercicio2/Programa.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio2
+{
+    public partial class Frm_P6 : Form
+    {
+        public Frm_P6()
+        {
+            InitializeComponent();
+        } 
+        private void frm_programa_Load(object sender, EventArgs e)
+        {
+            Cbbox_operaciones.Items.Add("NUMEROS PRIMOS");
+            Cbbox_operaciones.Items.Add("NUMEROS PERFECTOS");
+            Cbbox_operaciones.Items.Add("SERIE FIBONACCI");
+
+
+        }
+
+        private void btn_genenar_Click(object sender, EventArgs e)
+        {
+            lbx_num.Items.Clear();
+            int Cantidad = Convert.ToInt32(txt_numero.Text);
+            string Elegir = Cbbox_operaciones.Text;
+            if (Elegir == "NUMEROS PRIMOS")
+            {
+                int cont = 0;
+                while(cont < Cantidad)
+                for (int i = 2; i <= 100000; i++)
+                {
+                    bool esPrimo = true;
+                    
+                    for (int j = 2; j <= Math.Sqrt(i); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            esPrimo = false;
+                            break;
+                        }
+                    }
+
+                    if (esPrimo)
+                    {
+                        lbx_num.Items.Add(i);
+                            cont++;
+                            if (cont == Cantidad)
+                            {
+                                i = 1000000;
+                            }
+                    }
+                    
+                }
+            }
+            
+            if (Elegir == "NUMEROS PERFECTOS")
+            {
+                int num = 0;
+                int cont = 0;
+                while (num < 1000000)
+                {
+                    if (esPerfecto(num))
+                    {
+                        lbx_num.Items.Add(num);
+
+                        cont++;
+                        if (cont == Cantidad) 
+                        {
+                            num = 1000000;
+                        }
+                    }
+                    num++;
+                }
+            }
+            if (Elegir == "SERIE FIBONACCI")
+            {
+                int v1 = 0;
+                int v2 = 1;
+
+                for (int i = 0; i < Cantidad; i++)
+                {
+                    int temp = v1;
+
+                    v1 = v2;
+                    v2 = temp + v1;
+                    lbx_num.Items.Add(v2);
+                }
+            }
+
+        }
+
+        static bool esPerfecto(int n)
+        {
+            int suma = 0;
+            int i = 1;
+            while (i < n)
+            {
+                if (n % i == 0) 
+                {
+                    suma += i;
+                    if (suma > n) return false;
+                }
+                i++;
+            }
+            return (suma == n);
+        }
+    }
+}
